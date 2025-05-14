baee10d030b607073f33fe431f915000545e2bb6
Sylvain Labopin <sylvain.labopin@gmail.com>
2025-05-14 11:18:12 +0200

    feat(osal): implement OS abstraction layer

---
+++ src/osal/include/osal.h
@@ -0,0 +1,14 @@
+// src/osal/include/osal.h
+
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+    /// Pause the thread for ms milliseconds.
+    void osal_sleep(int ms);
+
+#ifdef __cplusplus
+}
+#endif
