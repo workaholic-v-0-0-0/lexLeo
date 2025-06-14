6c561543888e0fe4b0a00257d3d57a6998131ef3
Sylvain Labopin <sylvain.labopin@gmail.com>
2025-06-14 11:57:34 +0200

    WIP

--- src/data_structures/src/hashtable.c
+++ src/data_structures/src/hashtable.c
@@ -37,9 +37,34 @@ hashtable *hashtable_create(size_t size, hashtable_destroy_value_fn_t destroy_va
     return ret;
 }
 
+
+// pb: destroy_fn_t does not know how to destroy entry cause it depends on how to destroy entry.value
+// we need a fct which map a fct destroying an entity of type t to a fct destroying an entry type with value field of type t
+static void hashtable_destroy_entry(entry * e, void (*destroy_fn_t)(void *)) {
+    DATA_STRUCTURE_FREE(e->key);
+    if (destroy_value_fn) destroy_value_fn(e->value);
+}
+
+// precondition: ht null or correctly initialized
 void hashtable_destroy(hashtable *ht) {
     if (!ht)
         return;
+    for (size_t i = 0 ; i < ht->size ; i++) {
+        if ((ht->buckets)[i]) {
+            list_free_list(
+                (ht->buckets)[i],
+                hashtable_destroy_entry
+            );
+
+/*
+            DATA_STRUCTURE_FREE(((entry *) ((ht->buckets)[i])->car)->key);
+            if (ht->destroy_value_fn)
+                (ht->destroy_value_fn)(((entry *) ((ht->buckets)[i])->car)->value);
+            DATA_STRUCTURE_FREE(((ht->buckets)[i])->car);
+            DATA_STRUCTURE_FREE((ht->buckets)[i]);
+*/
+        }
+    }
 	DATA_STRUCTURE_FREE(ht->buckets);
 	ht->buckets = NULL;
 	DATA_STRUCTURE_FREE(ht);
