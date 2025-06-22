3526f738c3e7c2566005e40116f62b4060bba86d
Sylvain Labopin <sylvain.labopin@gmail.com>
2025-06-22 19:21:05 +0200

    feat(ast): Implementation of ast_create_typed_data_string via TDD

--- src/ast/src/ast.c
+++ src/ast/src/ast.c
@@ -3,6 +3,7 @@
 #include "ast.h"
 
 #include "internal/ast_memory_allocator.h"
+#include "internal/ast_string_utils.h"
 
 #include <stdlib.h>
 
@@ -20,4 +21,21 @@ typed_data *ast_create_typed_data_int(int i) {
 
 void ast_destroy_typed_data_int(typed_data *typed_data_int) {
     AST_FREE(typed_data_int);
+}
+
+typed_data *ast_create_typed_data_string(char *s) {
+    typed_data *ret = AST_MALLOC(sizeof(typed_data));
+    if (!ret)
+        return NULL;
+
+    char *string_value = AST_STRING_DUPLICATE(s);
+    if (!string_value) {
+        AST_FREE(ret);
+        return NULL;
+    }
+
+    ret->type = TYPE_STRING;
+    ret->data.string_value = string_value;
+
+    return ret;
 }
