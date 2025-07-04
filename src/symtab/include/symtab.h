96f8b0b1a86ab054a0fc02bda1268462ba137829
Sylvain Labopin <sylvain.labopin@gmail.com>
2025-07-04 17:37:19 +0200

    feat(symtab/interface): Add local and global versions of functions symtab_get, symtab_reset and symtab_contains

--- src/symtab/include/symtab.h
+++ src/symtab/include/symtab.h
@@ -28,5 +28,8 @@ symbol *symtab_get_local(symtab *st, const char *name); // the caller is respons
 int symtab_reset_local(symtab *st, const char *name, ast *image);// the caller is responsible for passing either NULL or a well-formed symtab and ast pointers
 int symtab_remove(symtab *st, const char *name); // the caller is responsible for passing either NULL or a well-formed symtab pointer
 int symtab_contains_local(symtab *st, const char *name);
+symbol *symtab_get(symtab *st, const char *name); // the caller is responsible for passing either NULL or a well-formed symtab pointer
+int symtab_reset(symtab *st, const char *name, ast *image);// the caller is responsible for passing either NULL or a well-formed symtab and ast pointers
+int symtab_contains(symtab *st, const char *name);
 
 #endif //SYMTAB_H
